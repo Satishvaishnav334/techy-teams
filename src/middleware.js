15775e2e0bdcb39e708c31ee2e9e8fb356d31d8e
arti <arti@techysquad.com>
2025-08-13 18:55:41 +0530

    final dat

--- src/middleware.js
+++ src/middleware.js
@@ -1,37 +1,43 @@
+
+
 import { NextResponse } from 'next/server';
+import jwt from 'jsonwebtoken'
+    import { jwtVerify } from 'jose';
+export function middleware(request, res) {
+    const { pathname } = request.nextUrl;
+    try {
+        const protectedPaths = ['/dashboard'];
+        const protectedAdminPaths = ['/dashboard/admin'];
+        if (protectedPaths.some(path => pathname.startsWith(path))) {
+            const token = request.cookies.get('token');
+            const clearToken = token.value;
+            if (!clearToken) {
+                return NextResponse.redirect(new URL('/login', request.url));
+            }
+            const decode2 = jwt.decode(clearToken)
+            console.log(decode2)
+            // jwtVerify(token, process.env.JWT_SECRET);
+        
+            return NextResponse.next();
+        }
 
-export function middleware(request) {
-  const { pathname } = request.nextUrl;
-
-  // Define paths that require authentication
-  const protectedPaths = ['/dashboard'];
-  const protectedAdminPaths = ['/dashboard/admin'];
-
-  // Check if the current path is a protected path
-  if (protectedPaths.some(path => pathname.startsWith(path))) {
-    const authToken = request.cookies.get('token');
-    if (!authToken) {
-      return NextResponse.redirect(new URL('/login', request.url));
     }
-  }
-  if (protectedAdminPaths.some(path => pathname.startsWith(path))) {
-    const authToken = request.cookies.get('token');
-    
-    if (!authToken) {
-      return NextResponse.redirect(new URL('/login', request.url));
+    catch (error) {
+        console.log(error)
+        return NextResponse.redirect(new URL('/login', request.url));
     }
-  }
 
-  // If authenticated or not a protected path, allow the request to proceed
-  return NextResponse.next();
+
+
+
+
 }
-
-// Configure the matcher to run middleware on specific paths
 export const config = {
-  matcher: [
-    '/dashboard/:path*', // Match any path under /dashboard
-    '/profile/:path*',   // Match any path under /profile
-    // Exclude static files, API routes, and the login page itself
-    '/((?!api|_next/static|_next/image|favicon.ico|login).*)', 
-  ],
+    matcher: [
+        '/dashboard/:path*', // Match any path under /dashboard
+        '/profile/:path*',   // Match any path under /profile
+        // Exclude static files, API routes, and the login page itself
+        '/((?!api|_next/static|_next/image|favicon.ico|login).*)',
+    ],
 };
+
