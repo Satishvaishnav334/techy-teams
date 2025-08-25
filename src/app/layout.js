14cef75469d870e572f7683eb3b6f415b5c29a6e
arti <arti@techysquad.com>
2025-08-25 17:50:55 +0530

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,25 @@ import Navbar from "../components/Navbar.jsx";
 import Footer from "../components/Footer.jsx";
 import { LoadingProvider } from "@/components/context/LoadingContext";
 import { Toaster } from "@/components/ui/sonner"
-import Notifications from "@/components/Notification";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
 export const metadata = {
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+    ],
+  },
   title: "Techysquad Teams",
   description: "Created By Techysquad Team",
 };
@@ -13,7 +30,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body  className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <main>
           <LoadingProvider>
             <Navbar />
@@ -26,3 +43,7 @@ export default function RootLayout({ children }) {
     </html >
   );
 }
+
+
+
+
