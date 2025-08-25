962baaf837fb98d1e9c35f45076c3e68b904f0a5
arti <arti@techysquad.com>
2025-08-25 19:01:27 +0530

    update add task form design

--- src/app/admin/dashboard/manage-tasks/add-task/page.jsx
+++ src/app/admin/dashboard/manage-tasks/add-task/page.jsx
@@ -7,10 +7,12 @@ import { useAdminContext } from '@/components/context/AdminContext'
 import { DatePicker } from "@/components/ui/date-picker"
 import { toast } from 'sonner'
 import { useLoadingContext } from '@/components/context/LoadingContext'
+import { Button, Input, Textarea, Typography } from "@material-tailwind/react";
+
 function page() {
-  
+
   const { users, refresh, } = useAdminContext()
-  const {user,createNotification} = useLoadingContext()
+  const { user, createNotification } = useLoadingContext()
   const router = useRouter()
   const [title, setTitle] = useState()
   const [desc, setDesc] = useState()
@@ -34,14 +36,14 @@ function page() {
       console.log(date)
 
       const create = await axios.post('/api/get-tasks', formData)
-      console.log(create) 
-      if(create.status=='200'){
+      console.log(create)
+      if (create.status == '200') {
         createNotification(` New task ${title} Create by ${user.name}`)
         refresh()
         router.push('/admin/dashboard/manage-tasks')
-      } 
+      }
     } catch (error) {
-      toast.error("Error creating Task ",{closeButton:true})
+      toast.error("Error creating Task ", { closeButton: true })
       console.error('Error creating Task :', error);
     }
     finally {
@@ -55,67 +57,93 @@ function page() {
 
 
   return (
-    <div className="  flex flex-col items-center justify-center min-h-screen text-black bg-gray-100">
-      <h1 className="text-3xl font-bold mb-6">Create Task</h1>
-      <form onSubmit={handleCreate} className="p-4 m-2 bg-white rounded shadow-md">
-        <label className="block font-semibold text-2xl  my-1">Team Title</label>
-        <input
-          type="text"
-          required={true}
-          className="border border-gray-600 text-xl rounded-2xl w-full p-2"
-          placeholder="Enter Task Name or Title *"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
+
+    <div className="  flex flex-col items-center justify-center h-[90vh] text-black ">
+      {/* <h1 className="text-3xl font-bold mb-6">Create Task </h1> */}
+      <form onSubmit={handleCreate} className=" m-2 bg-gray-100 rounded-xl shadow-md shadow-black/50 p-5 flex flex-col gap-3">
+        <h1 className=" text-center text-2xl !text-gray-900 font-bold " >
+          Create New Task
+        </h1>
+        <Typography  variant="xl" className="mb-2 text-left font-medium !text-gray-900" >
+          Task Title
+        </Typography>
+        <Input type="text" required={true} className=" text-xl focus:border-t-gray-900 rounded-lg w-full p-2" placeholder="Enter Task Name or Title *" value={title}
+          onChange={(e) => setTitle(e.target.value)} color="gray" size="xl" name="task-title"
+          containerProps={{
+            className: "min-w-full",
+          }}
+          labelProps={{
+            className: "hidden",
+          }}
         />
-        <label className="block font-semibold text-2xl  my-1">Description</label>
-        <textarea
-          required={true}
-          type="text"
-          className="border border-gray-600 text-xl rounded-2xl w-full p-2"
-          placeholder="Enter Task Description *"
-          value={desc}
-          onChange={(e) => setDesc(e.target.value)}
-        />
-        <label className="block font-semibold text-2xl  my-1">Task Priority</label>
-        <select className="border border-gray-600 text-xl rounded-2xl  p-2" onChange={(e) => setPriority(e.target.value)}>
-          <option value=""> Priority default Medium</option>
-          <option value="Important">Important</option>
-          <option value="Medium">Medium</option>
-          <option value="Low">Low </option>
-        </select>
-
-        <label className="block font-semibold text-2xl  my-1">Status</label>
-        <select className="border border-gray-600 text-xl rounded-2xl  p-2" onChange={(e) => setStatus(e.target.value)}>
-          <option value=""> Status default Pending</option>
-          <option value="pending">Pending</option>
-          <option value="in-progress">In-progress</option>
-          <option value="completed">completed </option>
-        </select>
-
-
-        <label className="block font-semibold text-2xl  my-1">Due Date</label>
-        <DatePicker
-          date={date}
-          onDateChange={setDate}
-        />
-
-
-
-        <div className="p-5 text-2xl rounded-lg border my-4 w-full">
-          <h2 className="text-lg font-semibold mb-3">Select User you Want to Assign Task</h2>
-          <div className="grid grid-cols-2 gap-3 ">
+        <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 text-gray-600  w-full '>
+          <div className='w-full flex flex-col items-center justify-center'>
+            <Typography variant="xl" className="my-2 text-left font-medium !text-gray-900" >
+              Task Due Date
+            </Typography>
+            <DatePicker
+              date={date}
+              onDateChange={setDate}
+            />
+          </div>
+          <div className='w-full flex flex-col items-center justify-center'>
+            <Typography variant="xl" className="my-2 text-left font-medium !text-gray-900" >
+              Assign Task To
+            </Typography>
             <label className="flex items-center space-x-2">
               <select className="border border-gray-600 text-xl rounded-2xl  p-2" required={true} value={assignedTo} onChange={(e) => setAssignedTo(e.target.value)}>
                 <option value="">No User Selected</option>
                 {users?.map((member, index) => {
                   return (
-                    <option  key={index} value={member._id}>{member.name}</option>
+                    <option key={index} value={member._id}>{member.name}</option>
                   );
                 })}
               </select>
             </label>
           </div>
         </div>
+        <Typography variant="xl" className="my-2 text-left font-medium !text-gray-900" >
+          Task Description
+        </Typography>
+        <Textarea required={true} value={desc} rows={6} color="gray" placeholder="Enter Task Description *" name="message" className="focus:border-t-gray-900  text-xl rounded-lg w-full p-2"
+          onChange={(e) => setDesc(e.target.value)}
+          containerProps={{
+            className: "!min-w-full",
+          }}
+          labelProps={{
+            className: "hidden",
+          }}
+        />
+
+        <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 text-gray-600  w-full '>
+          <div className='w-full flex flex-col items-center justify-center'>
+            <Typography variant="xl" className="my-2 text-left font-medium !text-gray-900" >
+              Task Priority
+            </Typography>
+            <select className="border border-gray-400 text-xl rounded-lg  p-2 " onChange={(e) => setPriority(e.target.value)} >
+              <option value="" className=' border border-gray-400 '> Medium</option>
+              <option value="Important">Important</option>
+              {/* <option value="Medium">Medium</option> */}
+              <option value="Low">Low </option>
+            </select>
+          </div>
+          <div className='w-full flex flex-col items-center justify-center'>
+            <Typography variant="xl" className="my-2 text-left font-medium !text-gray-900" >
+              Task Status
+            </Typography>
+            <select className="border border-gray-400 text-xl rounded-lg  p-2" onChange={(e) => setStatus(e.target.value)}>
+              <option value="">  Pending</option>
+              {/* <option value="pending">Pending</option> */}
+              <option value="in-progress">In-progress</option>
+              <option value="completed">completed </option>
+            </select>
+          </div>
+        </div>
+
+
+
+
+
         <button className="bg-blue-600 font-semibold text-white px-3 py-2 my-2 rounded-lg text-xl" type="submit">Create Task</button>
       </form>
 
--- src/components/ui/date-picker.jsx
+++ src/components/ui/date-picker.jsx
@@ -23,7 +23,7 @@ export function DatePicker({
         <Button
           variant={"outline"}
           className={cn(
-            "w-[280px] justify-start text-left p-5 text-xl text-black font-normal",
+            " justify-start text-left p-5 text-xl text-black font-normal",
             !date && "text-muted-foreground",
             className
           )}>
